9aaa042926b2d10efd848b849b083b9c6eb77e00
Robin Dousse <rdousse63@gmail.com>
2020-06-21 11:49:53 +0200

    [type] more typetraits

--- src/stbipp/TypeTraits.hpp
+++ src/stbipp/TypeTraits.hpp
@@ -12,6 +12,9 @@ struct PixelTypeTrait
 {
     typedef void type;
     static constexpr int size = 0;
+    static constexpr ImageFormat format8Bits = ImageFormat::RGB8;
+    static constexpr ImageFormat format16Bits = ImageFormat::RGB16;
+    static constexpr ImageFormat format32Bits = ImageFormat::RGB32;
 };
 
 template <>
@@ -19,6 +22,9 @@ struct PixelTypeTrait<ImageFormat::LUM8>
 {
     typedef unsigned char type;
     static constexpr int size = 1;
+    static constexpr ImageFormat format8Bits = ImageFormat::LUM8;
+    static constexpr ImageFormat format16Bits = ImageFormat::LUM16;
+    static constexpr ImageFormat format32Bits = ImageFormat::LUM32;
 };
 
 template <>
@@ -26,6 +32,9 @@ struct PixelTypeTrait<ImageFormat::LUMA8>
 {
     typedef unsigned char type;
     static constexpr int size = 2;
+    static constexpr ImageFormat format8Bits = ImageFormat::LUMA8;
+    static constexpr ImageFormat format16Bits = ImageFormat::LUMA16;
+    static constexpr ImageFormat format32Bits = ImageFormat::LUMA32;
 };
 
 template <>
@@ -33,6 +42,9 @@ struct PixelTypeTrait<ImageFormat::RGB8>
 {
     typedef unsigned char type;
     static constexpr int size = 3;
+    static constexpr ImageFormat format8Bits = ImageFormat::RGB8;
+    static constexpr ImageFormat format16Bits = ImageFormat::RGB16;
+    static constexpr ImageFormat format32Bits = ImageFormat::RGB32;
 };
 
 template <>
@@ -40,6 +52,9 @@ struct PixelTypeTrait<ImageFormat::RGBA8>
 {
     typedef unsigned char type;
     static constexpr int size = 4;
+    static constexpr ImageFormat format8Bits = ImageFormat::RGBA8;
+    static constexpr ImageFormat format16Bits = ImageFormat::RGBA16;
+    static constexpr ImageFormat format32Bits = ImageFormat::RGBA32;
 };
 
 template <>
@@ -47,6 +62,9 @@ struct PixelTypeTrait<ImageFormat::LUM16>
 {
     typedef unsigned short type;
     static constexpr int size = 1;
+    static constexpr ImageFormat format8Bits = ImageFormat::LUM8;
+    static constexpr ImageFormat format16Bits = ImageFormat::LUM16;
+    static constexpr ImageFormat format32Bits = ImageFormat::LUM32;
 };
 
 template <>
@@ -54,6 +72,9 @@ struct PixelTypeTrait<ImageFormat::LUMA16>
 {
     typedef unsigned short type;
     static constexpr int size = 2;
+    static constexpr ImageFormat format8Bits = ImageFormat::LUMA8;
+    static constexpr ImageFormat format16Bits = ImageFormat::LUMA16;
+    static constexpr ImageFormat format32Bits = ImageFormat::LUMA32;
 };
 
 template <>
@@ -61,6 +82,9 @@ struct PixelTypeTrait<ImageFormat::RGB16>
 {
     typedef unsigned short type;
     static constexpr int size = 3;
+    static constexpr ImageFormat format8Bits = ImageFormat::RGB8;
+    static constexpr ImageFormat format16Bits = ImageFormat::RGB16;
+    static constexpr ImageFormat format32Bits = ImageFormat::RGB32;
 };
 
 template <>
@@ -68,6 +92,9 @@ struct PixelTypeTrait<ImageFormat::RGBA16>
 {
     typedef unsigned short type;
     static constexpr int size = 4;
+    static constexpr ImageFormat format8Bits = ImageFormat::RGBA8;
+    static constexpr ImageFormat format16Bits = ImageFormat::RGBA16;
+    static constexpr ImageFormat format32Bits = ImageFormat::RGBA32;
 };
 
 template <>
@@ -75,6 +102,9 @@ struct PixelTypeTrait<ImageFormat::LUM32>
 {
     typedef float type;
     static constexpr int size = 1;
+    static constexpr ImageFormat format8Bits = ImageFormat::LUM8;
+    static constexpr ImageFormat format16Bits = ImageFormat::LUM16;
+    static constexpr ImageFormat format32Bits = ImageFormat::LUM32;
 };
 
 template <>
@@ -82,6 +112,9 @@ struct PixelTypeTrait<ImageFormat::LUMA32>
 {
     typedef float type;
     static constexpr int size = 2;
+    static constexpr ImageFormat format8Bits = ImageFormat::LUMA8;
+    static constexpr ImageFormat format16Bits = ImageFormat::LUMA16;
+    static constexpr ImageFormat format32Bits = ImageFormat::LUMA32;
 };
 
 template <>
@@ -89,6 +122,9 @@ struct PixelTypeTrait<ImageFormat::RGB32>
 {
     typedef float type;
     static constexpr int size = 3;
+    static constexpr ImageFormat format8Bits = ImageFormat::RGB8;
+    static constexpr ImageFormat format16Bits = ImageFormat::RGB16;
+    static constexpr ImageFormat format32Bits = ImageFormat::RGB32;
 };
 
 template <>
@@ -96,6 +132,9 @@ struct PixelTypeTrait<ImageFormat::RGBA32>
 {
     typedef float type;
     static constexpr int size = 4;
+    static constexpr ImageFormat format8Bits = ImageFormat::RGBA8;
+    static constexpr ImageFormat format16Bits = ImageFormat::RGBA16;
+    static constexpr ImageFormat format32Bits = ImageFormat::RGBA32;
 };
 
 template <ImageFormat pixelFormat>
@@ -107,5 +146,12 @@ template <class DataType, int nbComponent>
 constexpr bool is_color_format_supported = (std::is_same<DataType, unsigned short>::value
         || std::is_same<DataType, unsigned char>::value
         || std::is_same<DataType, float>::value) && (nbComponent > 0) && (nbComponent < 5) ;
+
+template<ImageFormat pixelFormat>
+constexpr ImageFormat PixelTypeTraits_8Bits = PixelTypeTrait<pixelFormat>::format8Bits;
+template<ImageFormat pixelFormat>
+constexpr ImageFormat PixelTypeTraits_16Bits = PixelTypeTrait<pixelFormat>::format16Bits;
+template<ImageFormat pixelFormat>
+constexpr ImageFormat PixelTypeTraits_32Bits = PixelTypeTrait<pixelFormat>::format32Bits;
 
 }
