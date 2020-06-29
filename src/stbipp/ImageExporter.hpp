a435715d8f51a3633fa3fe57246907af721009da
Robin Dousse <rdousse63@gmail.com>
2020-06-29 20:49:55 +0200

    [doc] precision to import export format

--- src/stbipp/ImageExporter.hpp
+++ src/stbipp/ImageExporter.hpp
@@ -19,6 +19,13 @@ enum class ImageSaveFormat
 
 /**
  * @brief Save the given image at the given path with the specified format
+ *  * Stbipp uses the same save function you'll find in stb_image_write meaning that you are able to load the same file
+ * format :
+ * - JPEG
+ * - PNG
+ * - BMP
+ * - TGA
+ * - HDR
  * @param[in] path Path to the image to save
  * @param[in] image The image containing the data to save
  * @param[in] pixelFormat The pixel format to use
--- src/stbipp/ImageImporter.hpp
+++ src/stbipp/ImageImporter.hpp
@@ -9,6 +9,18 @@ namespace stbipp
 {
 /**
  * @brief Load an image at the given path with the given pixel format
+ * Stbipp uses the same load function you'll find in stb_image meaning that you are able to load the same file format :
+ * - JPEG
+ * - PNG
+ * - BMP
+ * - PSD
+ * - TGA
+ * - GIF
+ * - HDR
+ * - PIC
+ * - PNM (.ppm and .pgm)
+ * But it also contains the same limitations that you can find at :
+ * https://github.com/nothings/stb/blob/master/stb_image.h
  * @param[in] path Path to the image to load
  * @param[out] image The image which will contains the data (all contained data will be erased)
  * @param[in] pixelFormat The pixel format to use
